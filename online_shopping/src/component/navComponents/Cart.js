75c6db75b5203852c452a6f169e8b096d78d97a7
Sajank28 <71338898+Sajank28@users.noreply.github.com>
2021-04-02 11:28:51 +0530

    Changes in cart

--- online_shopping/src/component/navComponents/Cart.js
+++ online_shopping/src/component/navComponents/Cart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import {AiOutlineShoppingCart } from 'react-icons/ai'
 import {VscAdd} from 'react-icons/vsc'
 import {GrSubtract, GrSubtractCircle} from 'react-icons/gr'
@@ -7,8 +8,28 @@ import { Button } from 'react-bootstrap';
 class Cart extends Component {
     render() {
         return (
+    //         <li className="collection-item avatar" >
+    //         <div className="item-img"> 
+    //            <img src="/" alt="/" className=""/>
+    //         </div>
+       
+    //         <div className="item-desc">
+    //             <span className="title">title</span>
+    //             <p>desc</p>
+    //             <p><b>Price: $</b></p> 
+    //             <p>
+    //                 <b>Quantity:</b> 
+    //             </p>
+    //             <div className="add-remove">
+    //             <Button  variant="light"><Link to="/cart"><VscAdd/></Link></Button>
+    //             <Button variant="light"><Link to="/cart"><GrSubtract/></Link></Button>
+    //             </div>
+    //             <button className="waves-effect waves-light btn pink remove">Remove</button>
+    //         </div>
+           
+    //    </li>
             <div>
-                <h1><AiOutlineShoppingCart size="1.5em"/>My Cart (total items)---------------------(Clear my cart) </h1>
+                <h1><AiOutlineShoppingCart size="1.5em"/>My Cart (total items)---------------------<Button variant="dark" >Clear Cart</Button> </h1>
                 <hr/>
                 <p> Image-------------------------Product Description
                     <br/>
@@ -27,7 +48,10 @@ class Cart extends Component {
                 <hr/>
                 
             </div>
-        );
+
+            
+         );
+
     }
 }
 
