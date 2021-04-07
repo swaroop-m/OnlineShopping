b9d8c5cfb1e72a21c1cfcecafa363c4e69498111
swaroop-m <swaroopm91299@gmail.com>
2021-04-07 17:45:23 +0530

    commented hole carousel for now

--- online_shopping/src/component/navComponents/Home.js
+++ online_shopping/src/component/navComponents/Home.js
@@ -11,7 +11,7 @@ function Home(props) {
 
                 <br/>
                 
-                <CarouselOnHome />
+                {/* <CarouselOnHome /> */}
                 <br />
                 <CardsOnHome />
 
