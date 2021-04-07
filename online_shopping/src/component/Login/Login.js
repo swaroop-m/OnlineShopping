e8a17ddcd56aa788038bd8a755d1191a76a4d271
SafiyaSeher <seher.safia@gmail.com>
2021-04-07 23:58:04 +0530

    Changes in Login

--- online_shopping/src/component/Login/Login.js
+++ online_shopping/src/component/Login/Login.js
@@ -1,30 +1,83 @@
-import React from 'react'
-import { Form, Button} from 'react-bootstrap'
+import React,{Component} from 'react'
+import { Row,Col,Card,Form,InputGroup,FormControl,Button} from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSignInAlt,faEnvelope,faLock,faUndo} from "@fortawesome/free-solid-svg-icons";
 
-function Login(props) {
-    
 
-    return (
-        <>
+class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state=this.initialState;
+    }
 
-        <h2>Login page</h2>
-        <hr/>
-        <Form>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
+    initialState = {
+        username:'', password:''
+    };
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
+    credentialChange = event => {
+        this.setState({
+            [event.target.name] : event.target.value
+        });
+    };
 
-            <Button variant="primary" type="submit">Login </Button> <Button variant="secondary" type="register">Register </Button>
-        </Form>
-            
-        </>
-    )
+    resetLoginForm = () => {
+        this.setState(() => this.initialState);
+    };
+
+    render () {
+        const {username,password}=this.state;
+        return (
+        <Row className="justify-content-md-center">
+            <Col xs={5}>
+                <Card className="border border-dark bg dark text-black">
+                    <Card.Header>
+                        <FontAwesomeIcon icon={faSignInAlt}/> Login
+                    </Card.Header>
+                    <Card.Body>
+                        <Form.Row>
+                            <Form.Group as={Col}>
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text>
+                                            <FontAwesomeIcon icon={faEnvelope}/>
+                                        </InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <FormControl required autoComplete="off" type="text" name="username" value={username} onChange={this.credentialChange}
+                                    className={"bg-dark text-white"} placeholder="Enter Username"/>
+                                </InputGroup>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row>
+                            <Form.Group as={Col}>
+                                <InputGroup>
+                                    <InputGroup.Prepend>
+                                        <InputGroup.Text>
+                                            <FontAwesomeIcon icon={faLock}/>
+                                        </InputGroup.Text>
+                                    </InputGroup.Prepend>
+                                    <FormControl required autoComplete="off" type="password" name="password" value={password} onChange={this.credentialChange}
+                                    className={"bg-dark text-white"} placeholder="Enter Password"/>
+                                </InputGroup>
+                            </Form.Group>
+                        </Form.Row>
+                    </Card.Body>
+                    <Card.Footer style={{"text-align":"right"}}>
+                        <Button size="sm" type="button" variant="success" 
+                        disabled={this.state.username.length===0 || this.state.password.length===0}>
+                            <FontAwesomeIcon icon={faSignInAlt}/> Login
+                        </Button >{' '}
+                        <Button size="sm" type="button" variant="info" onClick={this.resetLoginForm}
+                        disabled={this.state.username.length===0 && this.state.password.length===0}>
+                            <FontAwesomeIcon icon={faUndo}/> Reset
+                        </Button>
+                    </Card.Footer>
+                </Card>
+            </Col>
+        </Row>
+
+      
+        );
+    }
 }
 
-export default Login
+export default Login;
